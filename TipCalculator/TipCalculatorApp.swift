f7d5a8dc7bb0b567ce209e4ff24e1982791074c7
Tea Abuselidze <teona.abus@gmail.com>
2023-08-17 15:09:55 -0500

    Initial Commit

---
+++ TipCalculator/TipCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  TipCalculatorApp.swift
+//  TipCalculator
+//
+//  Created by Tea Abuselidze on 8/17/23.
+//
+
+import SwiftUI
+
+@main
+struct TipCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
